cb5713bb78d6311e7df1e9f95b8f95dcf2bef614
bo <610611577@qq.com>
2023-12-15 16:10:18 +0800

    specify the opencv library file path in cmake.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,18 +1,18 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include "QDebug"
 #include <iostream>
 extern "C"{
     #include "libavformat/avformat.h"
 }
-#include <opencv2/opencv.hpp>
+#include "opencv2/core/core.hpp"
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    qDebug()<<avformat_configuration();
-    std::cout << "OpenCV Version: " << cv::getVersionString() << std::endl;
+//    qDebug()<<avformat_configuration();
+    std::cout << "OpenCV Version: " << CV_VERSION << std::endl;
 }
 
 MainWindow::~MainWindow()
